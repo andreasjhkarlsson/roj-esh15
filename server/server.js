4d31f57f010b9fee54f197315b2e5f76b2d0c7c8
Andreas Karlsson <andreas.karlsson3@gmail.com>
2015-09-05 19:19:36 +0200

    Add sampling & simulator scripts

--- server/server.js
+++ server/server.js
@@ -8,6 +8,50 @@ var db = new sqlite3.Database('roj.db');
 
 var webappDir = "../webapp";
 
+function sendSensorCommand(ip,port,command,onSuccess) {
+    var client = new net.Socket();
+
+    
+    client.on("error", function(error) {
+       console.log("Couldn't connect to sensor "+ip+":"+port); 
+    });
+    
+    client.connect(port, ip, function() {
+        client.write(command);
+    });
+
+    client.on('data', function(data) {
+        onSuccess(data.toString());
+        client.destroy(); // kill client after server's response
+    }); 
+
+};
+
+function getCurrentDepth(ip,port,onSuccess) {
+    sendSensorCommand(ip,port,"R",function(response) {
+        var values = response.split(" ");
+        onSuccess ([parseFloat(values[0]),parseFloat(values[1])]);
+    });
+}
+
+function sampleAllStations() {
+    console.log("Start sampling round robin...");
+    db.each("SELECT * FROM STATION;",function(err,row) {
+        var addressComponents = row.ADDRESS.split(":");
+        var ip = addressComponents[0];
+        var port = parseInt(addressComponents[1]);
+        getCurrentDepth(ip,port,function(values) {
+            var meanValue = (values[0] + values[1]) / 2.0;
+            console.log(row.NAME+" reported depth of "+meanValue);
+            var statement = db.prepare("INSERT INTO READING(STATION,TIMESTAMP,DEPTH) VALUES(?,CURRENT_TIMESTAMP ,?)");
+            statement.run(row.ID,meanValue);
+            statement.finalize();
+        });
+    });
+}
+
+setInterval(sampleAllStations,10000);
+
 var app = express();
 
 app.use(express.static(__dirname + '/' + webappDir));
