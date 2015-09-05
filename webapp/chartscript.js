d0e3dcd0d3486f960c475fe4b14c6812f00a0f08
Charlie Forsgren <tjarles@Charlies-MacBook-Pro.local>
2015-09-05 20:07:55 +0200

    Charten fungerar! Dock är den hög och utan nederbörd

--- webapp/chartscript.js
+++ webapp/chartscript.js
@@ -2,17 +2,38 @@
  * Created by tjarles on 05/09/15.
  */
 
-new Chartist.Line('#chart1', {
-    labels: [1, 2, 3, 4],
-    series: [[100, 120, 180, 200]]
-});
-
 function drawChart(smhiData) {
-    console.log(smhiData);
+    //Lista med alla datum från SMHI
+    var dayList = [];
+    var snowList = [];
+    for (x in smhiData.timeseries) {
+        if (dayList[dayList.length-1] - dayList[0] > 5) {
+            new Chartist.Line('#chart1', {
+                labels: dayList,
+                series: [snowList]
+            });
+            break
+        }
+        var date = smhiData.timeseries[x].validTime;
+        //Ta ut datum för dagarna
+        var day = (date.substr(8, 2));
+        //Ta ut värdet från snöprognosen
+        var snow = smhiData.timeseries[x].pis;
+
+        dayList.push(day);
+        snowList.push(snow);
+
+
+
+
+
+
+
+    }
 }
 
 $(function() {
-    $.get("http://opendata-download-metfcst.smhi.se/api/category/pmp1.5g/version/1/geopoint/lat/58.41081/lon/15.62137/data.json",receiveSMHIData);
+    $.get("http://opendata-download-metfcst.smhi.se/api/category/pmp1.5g/version/1/geopoint/lat/58.41081/lon/15.62137/data.json",drawChart);
 });
 
 //
